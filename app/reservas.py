0202e9c7df04b4e66742e9ffbe849c3d7f082733
Consuelo <consuelogarrido.ab@gmail.com>
2025-06-23 00:03:59 -0400

    add missing def

--- app/reservas.py
+++ app/reservas.py
@@ -1,3 +1,11 @@
+reservas = []
+
+def verificar_disponibilidad(reservas, nueva):
+    for r in reservas:
+        if r["sala"] == nueva["sala"] and r["hora"] == nueva["hora"]:
+            return False
+    return True
+
 def reservar(nueva):
     if verificar_disponibilidad(reservas, nueva):
         reservas.append(nueva)
